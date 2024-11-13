d5a9b1da835e6b8d1b99386609fd40ae62e7e104
Osamah AlOsaimi <osamah.swe@gmail.com>
2024-11-14 02:09:44 +0300

    new class InvertedIndexBST added!
    TO DO :
    1-** fix method display **
    2-** revising whole class attributes and methods **

---
+++ src/InvertedIndexBST.java
@@ -0,0 +1,40 @@
+public class InvertedIndexBST {
+    BST<Word> invertedindexBST;
+    public InvertedIndexBST(){
+        invertedindexBST = new BST<Word>();
+    }
+    public void add(String text, int id){
+        if(!searchWordInInvertedIndex(text)){
+            Word word = new Word(text);
+            word.documentIDs.insert(id);
+            invertedindexBST.insert(text,word);
+        }
+        else{
+            Word wordExists = invertedindexBST.retrieve();
+            wordExists.addID(id);
+        }
+    }
+
+    public boolean searchWordInInvertedIndex(String w){
+        return invertedindexBST.findkey(w);
+    }
+    public void display() {
+        if (invertedindexBST == null) {
+            System.out.println("Null inverted index list");
+            return;
+        } else if (invertedindexBST.empty()) {
+            System.out.println("Empty inverted index list");
+            return;
+        } else {
+            invertedindexBST.findFirst();
+            while (!invertedindexBST.last()) {
+                invertedindexBST.retrieve().display();
+
+                invertedindexBST.findNext();
+            }
+            invertedindexBST.retrieve().display();
+        }
+
+
+    }
+}
