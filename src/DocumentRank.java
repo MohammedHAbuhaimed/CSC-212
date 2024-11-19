7e4a7a640595d2129b2cb45520e1944921d7de58
Osamah AlOsaimi <157825460+OsamahSwe@users.noreply.github.com>
2024-11-19 22:17:58 +0300

    New classes added ( Ranking , DocumentRank)!

---
+++ src/DocumentRank.java
@@ -0,0 +1,13 @@
+public class DocumentRank {
+    public int id;
+    public int rank;
+    public DocumentRank(int id,int rank){
+        this.id = id;
+        this.rank = rank;
+    }
+
+    public void display(){
+        System.out.printf("%-8d%-8d\n",id,rank);
+    }
+
+}
---
+++ src/Ranking.java
@@ -0,0 +1,174 @@
+import com.sun.jdi.connect.spi.TransportService;
+
+import javax.print.Doc;
+
+public class Ranking {
+    static String Query;
+    static InvertedIndexBST inverted;
+    static Index index;
+    static LinkedList<Integer> DocumentsInQuery;
+    static LinkedList<DocumentRank> DocumentRanked;
+
+public Ranking(InvertedIndexBST bst,Index i,String Q){
+    inverted = bst;
+    index = i;
+    Query = Q;
+    DocumentsInQuery = new LinkedList<Integer>();
+    DocumentRanked = new LinkedList<DocumentRank>();
+}
+
+public static void display(){
+    if(DocumentRanked.empty()){
+        System.out.println("Empty");
+        return;
+    }
+    System.out.printf("%-8s%-8s\n","DocumentID","Score");
+    DocumentRanked.findFirst();
+    while(!DocumentRanked.last()){
+    DocumentRanked.retrieve().display();
+    DocumentRanked.findNext();
+    }
+    DocumentRanked.retrieve().display();
+}
+
+
+
+public static Document getDocumentGivenID(int id){
+    return  index.           // should be fixed
+}
+
+
+
+public static int termCounterInDocument(Document d , String term){
+   int counter = 0;
+   LinkedList<String> words = d.words;
+   if(words.empty())
+       return 0;
+
+   words.findFirst();
+   while (!words.last()){
+       if(words.retrieve().equalsIgnoreCase(term))
+           counter++;
+       words.findNext();
+   }
+   if(words.retrieve().equalsIgnoreCase(term))
+       counter++;
+
+   return counter;
+}
+
+public static int getDocumentScore(Document d, String Query){
+    if(Query.length()==0)
+        return 0;
+    String terms[]= Query.split(" ");
+    int sumFrequency = 0;
+    for (int i = 0; i < terms.length; i++) {
+            sumFrequency+=termCounterInDocument(d,terms[i].trim().toLowerCase());
+    }
+return sumFrequency;
+}
+
+public static void RankQuery(){
+    LinkedList<Integer> L = new LinkedList<Integer>();
+    if(Query.length()==0) return;
+    String terms[]= Query.split(" ");
+    boolean found = false;
+    for (int i = 0; i < terms.length; i++) {
+            found=inverted.searchWordInInvertedIndex(terms[i].trim().toLowerCase());
+            if(found)
+                L=inverted.invertedindexBST.retrieve().documentIDs;
+            addingInOneListSorted(L);
+    }
+}
+
+public static void addingInOneListSorted(LinkedList<Integer>L){
+if(L.empty())
+    return;
+
+L.findFirst();
+    while (!L.last()) {
+        boolean found = existsInResult(DocumentsInQuery,L.retrieve());
+        if(!found){
+            insertSortedIDs(L.retrieve());
+        }
+        if(!L.last())
+            L.findNext();
+        else
+            break;
+    }
+}
+
+public static boolean existsInResult(LinkedList<Integer>result ,int id){
+    if(result.empty())
+        return false;
+
+    result.findFirst();
+    while (!result.last()) {
+        if (result.retrieve().equals(id)) {
+            return true;
+        }
+        result.findNext();
+    }
+    if(result.retrieve().equals(id))
+        return true;
+
+    return false;
+    }
+
+    public static void insertSortedIDs(Integer id){
+    if(DocumentsInQuery.empty()){
+        DocumentsInQuery.insert(id);
+        return;
+    }
+
+    DocumentsInQuery.findFirst();
+    while (!DocumentsInQuery.last()) {
+        if (id < DocumentsInQuery.retrieve()) {
+            Integer ID = DocumentsInQuery.retrieve();
+            DocumentsInQuery.update(id);
+            DocumentsInQuery.insert(id);
+            return;
+        } else
+            DocumentsInQuery.findNext();
+    }
+    if(id<DocumentsInQuery.retrieve()) {
+        Integer ID = DocumentsInQuery.retrieve();
+        DocumentsInQuery.update(id);
+        DocumentsInQuery.insert(id);
+        return;
+    }
+    else
+        DocumentsInQuery.insert(id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
